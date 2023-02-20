23cee0b1154e9df6decdc4ff7c29079e760bda7c
AnilAbhimanyu <anilkumarreddy3418@gmail.com>
2023-02-20 10:26:37 +0530

    venv unnecessary files removed

--- app.py
+++ app.py
@@ -6,7 +6,25 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'first page'
+    render_template('base.html')
+
+@app.route('/learn')
+def learn():
+    render_template('learn.html')
+
+@app.route('/projects')
+def projects():
+    render_template('projects.html')
+
+@app.route('/plans')
+def plans():
+    render_template('plans.html')
+
+@app.route('/notes')
+def notes():
+    render_template('notes.html')
+
+
 
 
 if __name__=="__main__":
