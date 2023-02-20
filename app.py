a62414cf6b02d09807fef7ae3a252ad9919937d8
AnilAbhimanyu <anilkumarreddy3418@gmail.com>
2023-02-20 18:35:56 +0530

    the nav bar created

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
 from flask import Flask,render_template
 
+# app=Flask(__name__,template_folder='template')
 app=Flask(__name__)
-
-
 
 @app.route('/')
 def index():
-    render_template('base.html')
+    return render_template('index.html')
 
 @app.route('/learn')
 def learn():
-    render_template('learn.html')
+    return render_template('learn.html')
 
 @app.route('/projects')
 def projects():
-    render_template('projects.html')
+    return render_template('projects.html')
 
 @app.route('/plans')
 def plans():
-    render_template('plans.html')
+    return render_template('plan.html')
 
 @app.route('/notes')
 def notes():
-    render_template('notes.html')
+    return render_template('notes.html')
 
+@app.route('/progress')
+def progress():
+    return render_template('progress.html')
 
 
 
